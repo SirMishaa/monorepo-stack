6bbb379ae59ed657f7007e5432c3fa971838cad6
Mishaa <mishaa.pro@proton.me>
2023-03-09 22:52:15 +0100

    :sparkles: initial commit, set up project

---
+++ apps/client-space/src/components/TheCounter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const properties = defineProps<{
+  initial: number
+}>()
+
+const { count, inc, dec } = useCounter(properties.initial)
+</script>
+
+<template>
+  <div>
+    {{ count }}
+    <button class="inc" @click="inc()">+</button>
+    <button class="dec" @click="dec()">-</button>
+  </div>
+</template>
